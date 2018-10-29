18e7ea65ac812c2a4a1e7d6c97112f619fac1cf6
Martin Morães <martin.moraes@outlook.com>
2018-10-29 13:19:34 -0300

    Incluido menuBar

--- src/TesteJFX/InicialController.java
+++ src/TesteJFX/InicialController.java
@@ -5,32 +5,31 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-import g_listView.Tipos;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
-public class InicialController implements Initializable{
+public class InicialController implements Initializable {
 // TesteJFX.InicialController
-    @FXML
-    private ListView<Painel> listView;	
-    @FXML
-    private BorderPane borderPane;
-    
-    private ArrayList<Painel> listaDePaineis = new ArrayList<>();
-    
-    
+	@FXML
+	private ListView<Painel> listView;
+	@FXML
+	private BorderPane borderPane;
+
+	private ArrayList<Painel> listaDePaineis = new ArrayList<>();
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		criaDadosListView();
-		
+
 		listView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Painel>() {
 			@Override
 			public void changed(ObservableValue<? extends Painel> arg0, Painel arg1, Painel arg2) {
@@ -39,50 +38,54 @@ public class InicialController implements Initializable{
 			}
 		});
 	}
-	
+
+	public void trataMenu(ActionEvent event) {
+		String menuClicado = ((MenuItem) event.getSource()).getText();
+		System.out.println(menuClicado);
+		for (Painel painel : listaDePaineis) {
+			if (painel.identificacao.equals(menuClicado))
+				borderPane.setCenter(carregaFXML(painel.FXML));
+		}
+	}
+
 	public void sair() {
 		Main.getPrimaryStage().close();
 	}
-    
 
-    // Trata o conteúdo do CENTRO
-	//===========================
-    
-    private Pane carregaFXML(String fxml) {
-    	try {
+	// Trata o conteúdo do CENTRO
+	// ===========================
+
+	private Pane carregaFXML(String fxml) {
+		try {
 			return FXMLLoader.load(getClass().getResource(fxml));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-    	return null;
-    }
-    
-    
-    
-	//Trata o conteúdo do ListView
-	//============================
-	
+		return null;
+	}
+
+	// Trata o conteúdo do ListView
+	// ============================
+
 	private void criaDadosListView() {
-		listaDePaineis.add(new Painel("Application","/a_application/FXMLInicial.fxml"));
-		listaDePaineis.add(new Painel("Manualmente","/b_manualmente/FXMLTelaPrincipal.fxml"));
-		listaDePaineis.add(new Painel("Combo Box","/c_comboBox/FXMLComboBox.fxml"));
-		listaDePaineis.add(new Painel("Check Box","/d_checkBox/FXMLCheckBox.fxml"));
-		listaDePaineis.add(new Painel("Radio Button","/e_radioButton/FXML.fxml"));
-		listaDePaineis.add(new Painel("Alertas","/f_alerta/FXML.fxml"));
-		listaDePaineis.add(new Painel("List View","/g_listView/FXML.fxml"));
-		listaDePaineis.add(new Painel("Table View","/h_tableView/FXML.fxml"));
+		listaDePaineis.add(new Painel("Application", "/a_application/FXMLInicial.fxml"));
+		listaDePaineis.add(new Painel("Manualmente", "/b_manualmente/FXMLTelaPrincipal.fxml"));
+		listaDePaineis.add(new Painel("Combo Box", "/c_comboBox/FXMLComboBox.fxml"));
+		listaDePaineis.add(new Painel("Check Box", "/d_checkBox/FXMLCheckBox.fxml"));
+		listaDePaineis.add(new Painel("Radio Button", "/e_radioButton/FXML.fxml"));
+		listaDePaineis.add(new Painel("Alertas", "/f_alerta/FXML.fxml"));
+		listaDePaineis.add(new Painel("List View", "/g_listView/FXML.fxml"));
+		listaDePaineis.add(new Painel("Table View", "/h_tableView/FXML.fxml"));
 //		listaDePaineis.add(new Painel("Nova Tela","/i_novasTelas/FXML.fxml"));
-		
+
 		listView.setItems(FXCollections.observableArrayList(listaDePaineis));
 	}
-	
-	    
-    
-    //@Descrição: Class ENTIDADE para cada página
-    private class Painel{
-    	private String identificacao;
-    	private String FXML;
-    	    	
+
+	// @Descrição: Class ENTIDADE para cada página
+	private class Painel {
+		private String identificacao;
+		private String FXML;
+
 		public Painel(String identificacao, String fXML) {
 			super();
 			this.identificacao = identificacao;
@@ -93,8 +96,6 @@ public class InicialController implements Initializable{
 		public String toString() {
 			return identificacao;
 		}
-    	
-    }
+
+	}
 }
-
-
